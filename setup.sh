9d0903eaf959eecbc0adb116f2ee54efefb7fe7b
Elliot Scher <ecscher84@gmail.com>
2024-07-16 10:01:03 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install curl git zip unzip tar cmake ninja-build build-essential libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config bison meson libx11-dev libxft-dev libxext-dev autoconf automake autoconf-archive libtool libxi-dev libxtst-dev nasm linux-libc-dev
+sudo apt-get install curl git zip unzip tar cmake ninja-build build-essential libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config bison meson libx11-dev libxft-dev libxext-dev autoconf automake autoconf-archive libtool libxi-dev libxtst-dev nasm linux-libc-dev libxml2 libxml2-dev
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.sh
