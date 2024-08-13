e1c0e17efb3ba3187b1ba23e138065ae57424966
ElliotScher <ecscher84@gmail.com>
2024-08-13 17:39:22 -0400

    add .ico and .icns

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -4,13 +4,12 @@
 
 #include <gtest/gtest.h>
 #include <cameracalibration.h>
-#include <nlohmann/json.hpp>
 
 TEST(OpenCV_Calibration, Typical)
 {
   const std::string projectRootPath = PROJECT_ROOT_PATH;
 
-  nlohmann::json ret = cameracalibration::calibrate(projectRootPath + "/assets/testcalibration.mp4", 0.709, 0.551, 12, 8, false);
+  int ret = cameracalibration::calibrate(projectRootPath + "/assets/testcalibration.mp4", 0.709, 0.551, 12, 8, false);
   EXPECT_EQ(ret, 0);
 }
 
@@ -18,17 +17,25 @@ TEST(OpenCV_Calibration, Atypical)
 {
   const std::string projectRootPath = PROJECT_ROOT_PATH;
 
-  nlohmann::json ret = cameracalibration::calibrate(projectRootPath + "assets/field video/PXL_20240730_204208090.mp4", 0.709, 0.551, 12, 8, true);
+  int ret = cameracalibration::calibrate(projectRootPath + "assets/field video/PXL_20240730_204208090.mp4", 0.709, 0.551, 12, 8, false);
   EXPECT_EQ(ret, 1);
 }
 
-// TEST(CameraCalibration, MRcal)
-// {
-//   const std::string projectRootPath = PROJECT_ROOT_PATH;
+TEST(MRcal_Calibration, Typical)
+{
+  const std::string projectRootPath = PROJECT_ROOT_PATH;
 
-//   nlohmann::json ret = cameracalibration::calibrate(projectRootPath + "/assets/testcalibration.mp4", 0.709, 12, 8, 1080, 1920, false);
-//   EXPECT_TRUE(ret.is_object());
-// }
+  int ret = cameracalibration::calibrate(projectRootPath + "/assets/testcalibration.mp4", 0.709, 12, 8, 1080, 1920, false);
+  EXPECT_EQ(ret, 0);
+}
+
+TEST(MRcal_Calibration, Atypical)
+{
+  const std::string projectRootPath = PROJECT_ROOT_PATH;
+
+  int ret = cameracalibration::calibrate(projectRootPath + "assets/field video/PXL_20240730_204208090.mp4", 0.709, 12, 8, 1080, 1920, false);
+  EXPECT_EQ(ret, 0);
+}
 
 int main(int argc, char **argv)
 {
